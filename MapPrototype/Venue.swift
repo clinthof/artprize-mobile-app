0fb844b132b1813b1650912c27eee83b47a4ae3b
Trevor Sweet <sweettre216@gmail.com>
2021-12-07 15:25:00 -0500

    Complete filling data structure with venue/artwork information

--- MapPrototype/Venue.swift
+++ MapPrototype/Venue.swift
@@ -14,18 +14,20 @@ import UIKit
 */
 struct Venue {
     let name : String?
+    let image : UIImage?
     let address : String?
     let location : CLLocationCoordinate2D?
     let artworks : [Artwork]?
     
-    init(name: String?, address: String?, location: CLLocationCoordinate2D?, artworks: [Artwork]?) {
+    init(name: String?, image: UIImage?, address: String?, location: CLLocationCoordinate2D?, artworks: [Artwork]?) {
         self.name = name
+        self.image = image
         self.address = address
         self.location = location
         self.artworks = artworks
     }
     
-    init() { self.init(name: nil, address: nil, location: nil, artworks: nil) }
+    init() { self.init(name: nil, image: nil, address: nil, location: nil, artworks: nil) }
 }
 
 /**
